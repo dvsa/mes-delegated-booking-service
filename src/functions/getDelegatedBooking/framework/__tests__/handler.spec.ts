29098e30ad370d37d52d98d355da43c15fd0431a
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-10-25 11:04:12 +0100

    AW SDK v3 upgrade (#62)

--- src/functions/getDelegatedBooking/framework/__tests__/handler.spec.ts
+++ src/functions/getDelegatedBooking/framework/__tests__/handler.spec.ts
@@ -1,7 +1,6 @@
 import { Mock, It, Times } from 'typemoq';
-import { APIGatewayEvent, Context } from 'aws-lambda';
+import { APIGatewayEvent } from 'aws-lambda';
 const lambdaTestUtils = require('aws-lambda-test-utils');
-
 import { handler } from '../handler';
 import * as createResponse from '../../../../common/application/utils/createResponse';
 import * as FindDelegatedBooking from '../../../../common/application/delegated-booking/FindDelegatedBooking';
@@ -11,7 +10,6 @@ import { booking } from '../../../../common/application/service/__mocks__/test-d
 
 describe('getDelegatedBooking handler', () => {
   let dummyApigwEvent: APIGatewayEvent;
-  let dummyContext: Context;
   let createResponseSpy: jasmine.Spy;
 
   const moqFindDelegatedBooking = Mock.ofInstance(FindDelegatedBooking.findDelegatedBooking);
@@ -30,42 +28,41 @@ describe('getDelegatedBooking handler', () => {
       },
     });
     dummyApigwEvent.requestContext.authorizer = { examinerRole: 'DLG' };
-    dummyContext = lambdaTestUtils.mockContextCreator(() => null);
     spyOn(FindDelegatedBooking, 'findDelegatedBooking').and.callFake(moqFindDelegatedBooking.object);
   });
 
   describe('given the FindDelegatedBooking returns a booking', () => {
     it('should return a successful response with the booking', async () => {
       moqFindDelegatedBooking.setup(x => x(It.isAny())).returns(() => Promise.resolve(booking));
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith(booking);
     });
   });
   describe('given FindDelegatedBooking throws a DelegatedBookingNotFoundError error', () => {
     it('should return HTTP 404 NOT_FOUND', async () => {
       moqFindDelegatedBooking.setup(x => x(It.isAny())).throws(new DelegatedBookingNotFoundError());
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith({}, 404);
     });
   });
   describe('given the FindDelegatedBooking throws', () => {
     it('should respond with internal server error', async () => {
       moqFindDelegatedBooking.setup(x => x(It.isAny())).throws(new Error('Unable to retrieve delegated booking'));
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith('Unable to retrieve delegated booking', 500);
     });
   });
   describe('given there is no applicationReference provided', () => {
     it('should indicate a bad request', async () => {
       dummyApigwEvent.pathParameters = {};
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith('No applicationReference provided', 400);
     });
   });
   describe('given there app ref is in wrong format', () => {
     it('should indicate a bad request', async () => {
       dummyApigwEvent.pathParameters = { applicationReference: '123' };
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith('Invalid applicationReference provided', 400);
     });
   });
@@ -77,7 +74,7 @@ describe('getDelegatedBooking handler', () => {
       dummyApigwEvent.pathParameters = { applicationReference: '12345678910' };
       createResponseSpy.and.returnValue({ statusCode: 200 });
       moqFindDelegatedBooking.setup(x => x(It.isAny())).returns(() => Promise.resolve(booking));
-      await handler(dummyApigwEvent, dummyContext);
+      await handler(dummyApigwEvent);
       expect(createResponse.default).toHaveBeenCalledWith(booking);
       moqFindDelegatedBooking.verify(x => x(It.isValue(12345678910)), Times.once());
     });
@@ -89,7 +86,7 @@ describe('getDelegatedBooking handler', () => {
         };
         dummyApigwEvent.requestContext.authorizer = null;
         createResponseSpy.and.returnValue({ statusCode: 401 });
-        await handler(dummyApigwEvent, dummyContext);
+        await handler(dummyApigwEvent);
         expect(createResponse.default).toHaveBeenCalledWith('No delegated examiner role present in request', 401);
       });
     });
